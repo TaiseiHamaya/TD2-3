a40ea94c237d22fd2c64a2559190cf0e77f1d157
Mutuking <kawaguchi.2525.haruki@gmail.com>
2025-02-06 07:31:07 +0900

    回転中のund禁止

--- project/Application/Character/Player/PlayerManager.cpp
+++ project/Application/Character/Player/PlayerManager.cpp
@@ -126,11 +126,10 @@ void PlayerManager::update() {
 		emplace_log(player->move_start_position(), player->start_rotation());
 	}
 
-
-	// 親子関係の管理
-	manage_parent_child_relationship();
 	// 入力処理を受け付ける
 	handle_input();
+	// 親子関係の管理
+	manage_parent_child_relationship();
 	// 子供をプレイヤーの向かせる処理
 	set_child_rotate();
 	// パーティクルのオンオフの切り替え処理
@@ -195,7 +194,9 @@ void PlayerManager::update() {
 	if (stageSituation == 0 &&
 		!(player->get_state() != PlayerState::Idle || player->is_falling() || child->is_falling()) &&
 		moveLogger->can_undo() && Input::IsTriggerKey(KeyID::Z)) {
-		undo();
+		if (player->get_state() == PlayerState::Idle) {
+			undo();
+		}
 		undoAudio->restart();
 	}
 }
@@ -227,6 +228,9 @@ void PlayerManager::handle_input() {
 	}
 
 	if (child->is_falling()) {
+		return;
+	}
+	if (player->is_move()) {
 		return;
 	}
 
