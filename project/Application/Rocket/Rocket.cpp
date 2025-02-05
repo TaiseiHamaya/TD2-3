f8bf5f5d5dc6feb0dfcf5ed5b7e132bcf8f16a1d
testName <k023g0068@g.neec.ac.jp>
2025-02-05 18:31:29 +0900

    Rocketが飛び立って行く演出を追加

--- project/Application/Rocket/Rocket.cpp
+++ project/Application/Rocket/Rocket.cpp
@@ -3,13 +3,15 @@
 #include<Engine/Resources/Animation/NodeAnimation/NodeAnimationPlayer.h>
 
 Rocket::Rocket(const Vector3& pos) {
-	animatedMeshInstance = std::make_unique<AnimatedMeshInstance>("GoalObj.gltf", "Standby", false);
+	
 
+	animatedMeshInstance = std::make_unique<AnimatedMeshInstance>("GoalObj.gltf", "Standby", false);
+	init();
 	Vector3 newPos = pos;
 	newPos.y = 0.5f;
 	animatedMeshInstance->get_transform().set_translate(newPos);
-	init();
-
+	particlePos = newPos;
+	explosionEmitter->get_transform().set_translate(newPos);
 }
 
 Rocket::~Rocket() {}
@@ -18,9 +20,16 @@ void Rocket::init() {
 	animatedMeshInstance->get_animation()->reset_animation("Standby");
 	animatedMeshInstance->get_animation()->restart();
 	isResult = false;
+	isFailedFlag = false;
 
 	gushingEmitter = std::make_unique<ParticleEmitterInstance>("gushing.json", 128);
 	explosionEmitter = std::make_unique<ParticleEmitterInstance>("explosion.json", 128);
+	gushingEmitter->set_active(false);
+	explosionEmitter->set_active(false);
+	spwanParticle = false;
+	spwanParticle2 = false;
+	particleSpawnTime = 0;
+	explosionSpawnTime = 0;
 
 
 }
@@ -40,6 +49,8 @@ void Rocket::update(int state) {
 
 	animatedMeshInstance->update();
 
+	particleUpdate();
+
 }
 
 void Rocket::begin() {
@@ -57,7 +68,7 @@ void Rocket::begin_rendering() {
 
 #include <imgui.h>
 void Rocket::debug_update() {
-	ImGui::Begin("gushing");
+	/*ImGui::Begin("gushing");
 	gushingEmitter->debug_gui();
 	ImGui::End();
 	ImGui::Begin("Rocket");
@@ -65,7 +76,7 @@ void Rocket::debug_update() {
 	ImGui::End();
 	ImGui::Begin("explosion");
 	explosionEmitter->debug_gui();
-	ImGui::End();
+	ImGui::End();*/
 }
 #endif // _DEBUG
 
@@ -84,12 +95,47 @@ void Rocket::isClear() {
 	isResult = true;
 	animatedMeshInstance->get_animation()->reset_animation("Clear");
 	animatedMeshInstance->get_animation()->restart();
+	particleSpawnTime = 0;
 }
 
 void Rocket::isFailed() {
 	if (isResult)return;
 	isResult = true;
+	isFailedFlag = true;
 	animatedMeshInstance->get_animation()->reset_animation("Failed");
 	animatedMeshInstance->get_animation()->restart();
+
+}
+
+void Rocket::particleUpdate() {
+	if (!isResult) { return; }
+
+	particleSpawnTime += WorldClock::DeltaSeconds();
+
+	if (particleSpawnTime >= 0.5f) {
+		//発射のパーティクルを再生
+		if (!spwanParticle) {
+			spwanParticle = true;
+			gushingEmitter->set_active(true);
+			gushingEmitter->restart();
+		}
+		
+
+	}
+	particlePos.y = animatedMeshInstance->get_animation()->calculate_translate("Rocket").y;
+	gushingEmitter->get_transform().set_translate(particlePos);
+	if (isFailedFlag) {
+		explosionSpawnTime+= WorldClock::DeltaSeconds();
+
+		if (explosionSpawnTime > 2.8f) {
+			if (!spwanParticle2) {
+				explosionEmitter->set_active(true);
+				explosionEmitter->restart();
+				spwanParticle2 = true;
+				particlePos.y = 0.5f;
+				explosionEmitter->get_transform().set_translate(particlePos);
+			}
+		}
+	}
 
 }
--- project/Application/Rocket/Rocket.h
+++ project/Application/Rocket/Rocket.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include "Engine/Resources/Audio/AudioPlayer.h"
 #include <Engine/Module/World/Particle/ParticleEmitterInstance.h>
+#include "Engine/Runtime/WorldClock/WorldClock.h"
 
 
 class AnimatedMeshInstance;
@@ -22,6 +23,7 @@ public:
 
 	void isClear();
 	void isFailed();
+	void particleUpdate();
 private:
 	std::unique_ptr<AnimatedMeshInstance> animatedMeshInstance;
 	bool isResult;
@@ -29,5 +31,12 @@ private:
 	std::unique_ptr<ParticleEmitterInstance> gushingEmitter;
 	std::unique_ptr<ParticleEmitterInstance> explosionEmitter;
 
+	bool isFailedFlag;
+	float particleSpawnTime;
+	
+	float explosionSpawnTime;
+	bool spwanParticle;
+	bool spwanParticle2;
+	Vector3 particlePos;
 };
 
