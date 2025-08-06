625b1a125646ecae23f4138f4abc958da32a3536
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-08-06 11:54:15 +0900

    アウトラインのシェーダーが正しくない不具合を修正しました。

--- project/EngineResources/HLSL/Posteffect/Outline/Outline.PS.hlsl
+++ project/EngineResources/HLSL/Posteffect/Outline/Outline.PS.hlsl
@@ -46,8 +46,8 @@ float4 main(PixelShaderInput input) : SV_TARGET0 {
 	
 	float weight = saturate(length(difference));
 	
-	if (weight > 0.0005f) {
-		return float4(0.0f, 0.0f, 0.0f, 1.0f);
+	if (weight > 0.00008f) {
+		return float4(0.21223075f, 0.0030352f, 0.283148f, 1.0f);
 	}
 	else {
 		return gTexture.Sample(gLinearSampler, input.texcoord);
